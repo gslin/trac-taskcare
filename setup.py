3f16894ca6b0b90127b44c25c03663b5809a11c3
Gea-Suan Lin <gslin@gslin.org>
2019-10-01 14:26:10 +0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TracTaskCare",
+    version="0.20191001.0",
+    author="Gea-Suan Lin",
+    author_email="darkkiller@gmail.com",
+    description="Sync updates to task.care.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gslin/trac-taskcare",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Apprived :: MIT License",
+        "Operating System :: OS :: Independent",
+    ],
+    python_requires=">=2.7",
+    entry_points={
+        "trac.plugins": "TracTaskCare = TracTaskCare"
+    },
+)
