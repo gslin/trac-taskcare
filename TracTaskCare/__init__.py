5941646137bf69d93d6bef0902552bcdfe265d1f
Gea-Suan Lin <gslin@gslin.org>
2019-10-02 13:30:18 +0800

    Add minimal implementation of ITicketChangeListener.

--- TracTaskCare/__init__.py
+++ TracTaskCare/__init__.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from trac.core import Component
+from trac.core import Component, implements
 
 class TracTaskCare(Component):
-    pass
+    implements(ITicketChangeListener)
+
+    def ticket_created(self, ticket):
+        pass
+
+    def ticket_changed(self, ticket, comment, author, old_values):
+        pass
+
+    def ticket_deleted(self, ticket):
+        pass
+
+    def ticket_comment_modified(self, ticket, cdate, author, comment, old_comment):
+        pass
+
+    def ticket_change_deleted(self, ticket, cdate, changes):
+        pass
