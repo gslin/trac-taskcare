7ec84eb9508571da4d01111dfbb25d2d6bfbbf98
Gea-Suan Lin <gslin@gslin.org>
2019-10-31 17:49:43 +0800

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TracTaskCare",
-    version="0.20191001.0",
+    version="0.20191031.0",
     author="Gea-Suan Lin",
     author_email="darkkiller@gmail.com",
     description="Sync updates to task.care.",
