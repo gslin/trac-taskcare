73354d54fa9c27845e9843f11c4c1ccab11cb14a
Gea-Suan Lin <gslin@gslin.org>
2019-10-31 12:28:06 +0800

    Read configurations.

--- TracTaskCare/__init__.py
+++ TracTaskCare/__init__.py
@@ -13,7 +13,18 @@ class TracTaskCare(Component):
         pass
 
     def ticket_changed(self, ticket, comment, author, old_values):
-        pass
+        auth_httpheader_key = self.taskcare.get('auth_httpheader_key')
+        auth_httpheader_value = self.taskcare.get('auth_httpheader_value')
+        auth_x_httpheader_key = self.taskcare.get('auth_x_httpheader_key')
+        auth_x_httpheader_value = self.taskcare.get('auth_x_httpheader_value')
+        resource_addtasks = self.taskcare.get('resource_addtasks')
+        taskcare_column = self.taskcare.get('taskcare_column')
+
+        if taskcare_column is None or '' == taskcare_column:
+            return
+        taskcare_ticket_number = ticket[taskcare_column]
+        if taskcare_ticket_number is None or '' == taskcare_ticket_numer:
+            return
 
     def ticket_deleted(self, ticket):
         pass
